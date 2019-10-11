60ef6e9b1b3ef060f662720a41f33fcda8a900b6
thegecko <rob.moran@arm.com>
2019-10-11 13:11:13 +0100

    Find a free port for PyOCD telnet semihosting

--- src/pyocd-server.ts
+++ src/pyocd-server.ts
@@ -24,13 +24,34 @@
 */
 
 import { AbstractServer } from './abstract-server';
+import { PortScanner } from './port-scanner';
 
 const LAUNCH_REGEX = /GDB server started/;
 const ERROR_REGEX = /:ERROR:gdbserver:/;
 const PERCENT_MULTIPLIER = 100 / 40; // pyOCD outputs 40 markers for progress
 
 export class PyocdServer extends AbstractServer {
+
+    protected portScanner = new PortScanner();
     protected progress = 0;
+
+    protected async resolveServerArguments(serverArguments?: string[]): Promise<string[]> {
+        if (!serverArguments) {
+            serverArguments = [];
+        }
+
+        const telnetPort = await this.portScanner.findFreePort(4444);
+
+        if (!telnetPort) {
+            return serverArguments;
+        }
+
+        return [
+            ...serverArguments,
+            '--telnet-port',
+            telnetPort.toString()
+        ];
+    }
 
     protected onStdout(chunk: string | Buffer) {
         super.onStdout(chunk);
